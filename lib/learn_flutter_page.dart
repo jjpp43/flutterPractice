1299fd134392d78337b3a5f3f873c5fb1b1123e9
Junna_Park <jparkdev63@gmail.com>
2022-10-29 06:36:00 +0000

    Uploaded image to asset

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -22,6 +22,11 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
           icon: Icon(Icons.arrow_back_ios),
         ),
       ),
+      body: Column(
+        children: [
+          Image.asset('images/sky.png'),
+        ],
+      ),
     );
   }
 }
